29fdd25cc01460d590a41e212013e582a6742984
Jorge Muñoz Guerrero <131190211+JorgeMunoz1991@users.noreply.github.com>
2023-11-07 17:10:20 +0000

    terminado

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,123 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+	  store: {
+		contacts: [],
+		contactDetails: [],
+	  },
+	  actions: {
+		// Use getActions to call a function within a fuction
+		exampleFunction: () => {
+		  getActions().changeColor(0, "green");
 		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
+		createContact: async (name, email, address, phone) => {
+		  const agendaSlug = "lucia_belen2023";
+		  const newContact = {
+			full_name: name,
+			email: email,
+			agenda_slug: agendaSlug,
+			address: address,
+			phone: phone,
+		  };
+		  console.log(name, email, address, phone);
+  
+		  try {
+			await fetch("https://assets.breatheco.de/apis/fake/contact/", {
+			  method: "POST",
+			  headers: {
+				"Content-Type": "application/json",
+			  },
+			  body: JSON.stringify(newContact),
+			})
+			  .then((response) => response.json())
+			  .then((data) => {
+				setStore({ contacts: [...getStore().contacts, data] });
+  
+				console.log(data);
+			  });
+		  } catch (error) {
+			console.error("Error:", error);
+		  }
+		},
+		getUserContacts: async () => {
+		  const requestOptions = {
+			method: "GET",
+		  };
+  
+		  try {
+			const response = await fetch(
+			  "https://assets.breatheco.de/apis/fake/contact/agenda/lucia_belen2023",
+			  requestOptions
+			);
+			const result = await response.json();
+			setStore({ contacts: result });
+			console.log(result)
+  
+		  } catch (error) {
+			console.log("Error:", error);
+		  }
+		},
+		getContactInfo: async (id) => {
+		  const requestOptions = {
+			method: "GET",
+		  };
+  
+		  try {
+			const response = await fetch(
+			  `https://assets.breatheco.de/apis/fake/contact/${id}`,
+			  requestOptions
+			);
+			const result = await response.json();
+			console.log(result)
+			setStore({ contactDetails: result });
+		  } catch (error) {
+			console.log("Error:", error);
+		  }
+		},
+  
+		editContact: async (id, name, email, address, phone) => {
+		  const agendaSlug = "lucia_belen2023";
+		  const updateContact = {
+			full_name: name,
+			email: email,
+			agenda_slug: agendaSlug,
+			address: address,
+			phone: phone,
+		  };
+		  console.log(name, email, address, phone);
+  
+		  try {
+			await fetch(`https://assets.breatheco.de/apis/fake/contact/${id}`, {
+			  method: "PUT",
+			  headers: {
+				"Content-Type": "application/json",
+			  },
+			  body: JSON.stringify(updateContact),
+			})
+			  .then((response) => response.json())
+			  .then((data) => {
+				setStore({ contacts: [...getStore().contacts, data] });
+  
+				console.log(data);
+			  });
+		  } catch (error) {
+			console.error("Error:", error);
+		  }
+		},
+	   deleteContact: async (id) => {
+		const baseUrl = "https://assets.breatheco.de/apis/fake/contact/";
+		const deleteUrl = `${baseUrl}${id}`;
+		const contacts = getStore().contacts;
+  
+		const filteredContacts = contacts.filter(
+		  (contact) => contact.id !== id
+		);
+  
+		await fetch(deleteUrl, { method: "DELETE" });
+		setStore({ contacts: filteredContacts });
+	  },
+	  },
 	};
-};
-
-export default getState;
+  };
+  
+  export default getState;
+  
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,21 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../store/appContext";
+import ContactCard from "../component/contactCard.js";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+
+	useEffect (() => {
+		actions.getUserContacts()
+	}, [] );
+
+	const userContacts = store.contacts || [];
+	console.log(userContacts)
+	
+	return (
+	<div className="text-center mt-5 m-auto">
+		{userContacts?.map((contact)=> <li key={contact.id} className="list-unstyled"><ContactCard id={contact.id} name={contact.full_name} phone={contact.phone} email={contact.email} address={contact.address}  /></li> )}
 	</div>
-);
+	)
+};
